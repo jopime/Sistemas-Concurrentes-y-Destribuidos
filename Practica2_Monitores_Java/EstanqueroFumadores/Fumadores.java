aa05b1f78165079ebdd63354c52ffc8904e2dd9d
jopime <jose.pimen.94@gmail.com>
2015-05-11 18:10:47 +0200

    Agregada condición de parada
    Agregada función para que tras un numero determinado de "Fumadas" el programa se detenga y las hebras se cierren correctamente

--- Practica2_Monitores_Java/EstanqueroFumadores/Fumadores.java
+++ Practica2_Monitores_Java/EstanqueroFumadores/Fumadores.java
@@ -21,8 +21,11 @@ class Estanco extends AbstractMonitor{
 	private Condition colaF2=makeCondition();
 
 	private int ingredienteActual=-1;
+	private int contador=0;
+	private boolean sigo=true;
+
 // invocado por cada fumador, indicando su ingrediente o numero
-	public void obtenerIngrediente( int miIngrediente, Thread thr){
+	public boolean obtenerIngrediente( int miIngrediente, Thread thr){
 		enter();
 		if (ingredienteActual!=miIngrediente){
 			switch(miIngrediente) {
@@ -31,10 +34,13 @@ class Estanco extends AbstractMonitor{
 				case 2: colaF2.await();	break;	
 			}		
 		}
+			if(sigo){
 			System.out.println(thr.getName()+", Comienza a fumar " + miIngrediente+miIngrediente);
 			ingredienteActual=-1;
 			colaE.signal();
+			}
 		leave();
+		return sigo;
 	}
 // invocado por el estanquero, indicando el ingrediente que pone
 	public void ponerIngrediente( int ingrediente , Thread thr){
@@ -45,19 +51,33 @@ class Estanco extends AbstractMonitor{
 				case 0: colaF0.signal(); break;
 				case 1: colaF1.signal(); break;
 				case 2: colaF2.signal();	break;	
-			}
-			
+				}
 		leave();
 	}
 // invocado por el estanquero
-	public void esperarRecogidaIngrediente(Thread thr){
+	public boolean esperarRecogidaIngrediente(Thread thr){
 		enter();
 			if(ingredienteActual!=-1) {
 				System.out.println(thr.getName()+", SIN INGREDIENTES ");
 				colaE.await();
 			}
+			contador++;
+			System.out.println(thr.getName()+", vez "+contador);
+			if(contador>14) sigo=false;
+		leave();
+		return sigo;
+	}
+	public void acabarConFumadores(){
+		enter();
+			while(!colaF0.isEmpty()) colaF0.signal();
+			System.out.println(", FINALIZANDOOO F0 "+colaF0.isEmpty());
+			while(!colaF1.isEmpty()) colaF1.signal();
+			System.out.println(", FINALIZANDOOO F0 "+colaF1.isEmpty());
+			while(!colaF2.isEmpty()) colaF2.signal();
+			System.out.println(", FINALIZANDOOO F0 "+colaF2.isEmpty());
 		leave();
 	}
+	public int contador(){return contador;}
 }
 
 //**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**
@@ -66,17 +86,19 @@ class Fumador implements Runnable{
 	private Estanco miestanco;
 	int miIngrediente;
 	public Thread thr ;
+	boolean cont=true;
 
 	public Fumador( Estanco estanco ,int p_miIngrediente ){ 
-		miIngrediente=p_miIngrediente;
+		miIngrediente=p_miIngrediente%3;
 		miestanco=estanco;
-		thr   = new Thread(this,"Fumador "+p_miIngrediente);
+		thr   = new Thread(this,"Fumador "+p_miIngrediente+"tipo "+miIngrediente);
 	}
 	public void run(){
-		while ( true ){
-			miestanco.obtenerIngrediente( miIngrediente ,thr);
+		while ( cont){
+			cont=miestanco.obtenerIngrediente( miIngrediente ,thr);
 			aux.dormir_max( 2000 );
-			System.out.println(thr.getName()+", TERMINA de fumar " + miIngrediente);
+			System.out.println(thr.getName()+", TERMINA de fumar " + miIngrediente+"cont? "+cont);
+			if(miestanco.contador()>14) cont=false;
 		}
 	}
 }
@@ -93,34 +115,38 @@ class Estanquero implements Runnable{
 	
 	public void run(){
 		int ingrediente ;
-		while (true){
+		boolean fin=true;
+		while (fin){
 			ingrediente = (int) (Math.random () * 3.0); // 0,1 o 2
 			miestanco.ponerIngrediente( ingrediente,thr );
-			miestanco.esperarRecogidaIngrediente(thr) ;
+			fin=	miestanco.esperarRecogidaIngrediente(thr) ;
+			if(!fin)
+				miestanco.acabarConFumadores();
+			}
 		}
 	}
-}
+
 
 
 //**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**
 
 class EjemploFumador{
 	public static void main( String[] args ) {
-
+		int tam=25;
 
     // leer parametros, crear vectores y buffer intermedio
-		Fumador[] fumadores = new Fumador[3] ;
+		Fumador[] fumadores = new Fumador[tam] ;
 		Estanco estanco = new Estanco();
     		Estanquero  estanquero = new Estanquero(estanco) ;
 
 	  // crear hebras
 
-		for (int i=0;i<3;i++)
+		for (int i=0;i<tam;i++)
 		    fumadores[i] = new Fumador(estanco,i);
 
 	  // poner en marcha las hebras
 		estanquero.thr.start();
-		for (int i=0;i<3;i++)
+		for (int i=0;i<tam;i++)
 			fumadores[i].thr.start();
 	}
 }
